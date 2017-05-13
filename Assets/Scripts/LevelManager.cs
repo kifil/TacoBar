84fe77459a030f460943c8784d4765649b808553
jmusselman <jmusselman@imagetrend.com>
2017-05-13 17:21:40 -0500

    Added Title screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public bool autoLoadNextLevel = false;
+	public float loadNextLevelAfter;
+
+	void Start()
+	{
+		if (autoLoadNextLevel) {
+			Invoke("LoadNextLevel", loadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
+
+	//Loads next level by index
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitRequest() {
+		Application.Quit();
+	}
+
+}
